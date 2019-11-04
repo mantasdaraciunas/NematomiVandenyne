fa35d69a1564252a8e0abbd8c5a3e83c8c5a12c0
mantasdaraciunas <mantas.daraciunas@hostinger.com>
2019-11-04 15:18:10 +0200

    Hand Picked products style update

--- mantas-plugin.php
+++ mantas-plugin.php
@@ -93,6 +93,14 @@ function prefix_custom_pre_get_posts_query( $q ) {
 	}
 }
 
+/**
+ * Remove the generated product schema markup from Product Category and Shop pages.
+ */
+function wc_remove_product_schema_product_archive() {
+	remove_action( 'woocommerce_shop_loop', array( WC()->structured_data, 'generate_product_data' ), 10, 0 );
+}
+add_action( 'woocommerce_init', 'wc_remove_product_schema_product_archive' );
+
 // add_filter( 'page_template', 'specialCategotry' );
 // function specialCategotry( $page_template )
 // {
