160fcd187f181b44b3306f0410adbdd3ba03d3d1
mantasdaraciunas <mantas.daraciunas@hostinger.com>
2020-04-30 10:18:09 +0300

    Add Structured seo data for page, post product_cat

--- includes/NVSeo.php
+++ includes/NVSeo.php
@@ -5,26 +5,56 @@ class NVSeo
 {
     function __construct()
     {
-
+        $this->register_hooks();
     }
 
-    function register_hooks() {
-        $post_types = get_post_types();
-
-        write_log($post_types);
-
-        foreach ( $post_types as $post_type ) {
-            add_filter( 'rest_prepare_' . $post_type->name, [ $this, 'rest_add_seo' ], 11, 3 );
-        }
-
-
-        add_filter( 'woocommerce_rest_prepare_product_cat', [ $this, 'rest_add_seo' ], 11, 3 );
+    function register_hooks()
+    {
+        add_filter('rest_prepare_post', [$this, 'rest_add_seo'], 11, 3);
+        add_filter('rest_prepare_page', [$this, 'rest_add_seo'], 11, 3);
+        add_filter('woocommerce_rest_prepare_product_cat',
+            [$this, 'rest_add_seo_taxonomy'], 11, 3);
     }
 
-    function rest_add_seo( $response, $post, $request ) {
+    function rest_add_seo($response, $post, $request)
+    {
+        $meta = get_post_meta($post->ID);
 
-        $response->data['nv_seo'] = "jega";
+        $response->data['nv_seo'] = $this->mapMetaData($meta);
 
         return $response;
+    }
+
+    function rest_add_seo_taxonomy($response, $post, $request)
+    {
+        $meta = get_term_meta($post->term_id);
+
+        $response->data['nv_seo'] = $this->mapMetaData($meta);
+
+        return $response;
+    }
+
+    private function mapMetaData($meta) {
+        if(!is_array($meta)) return [];
+
+        $seo = [
+            "title" => "",
+            "keywords" => "",
+            "description" => ""
+        ];
+
+        if(array_key_exists("seo-title", $meta)) {
+            $seo['title'] = $meta["seo-title"];
+        }
+
+        if(array_key_exists("seo-keywords", $meta)) {
+            $seo['keywords'] = $meta["seo-keywords"];
+        }
+
+        if(array_key_exists("seo-description", $meta)) {
+            $seo['description'] = $meta["seo-description"];
+        }
+
+        return $seo;
     }
 }
